bcf8874b14340d95dfec1c70d33eee3cda3c8b9e
Pavanzinho <dms.cerberus@hotmail.com>
2023-07-25 20:00:25 -0300

    feat:create component login

---
+++ src/app/admin/login/page.tsx
@@ -0,0 +1,21 @@
+'use client';
+import { AuthHeader } from '@/app/components/AuthHeader';
+import { SignInForm, Container, Main } from './styles';
+
+import AuthForm from '../../components/AuthForm';
+
+import Image from 'next/image';
+import { poppins, inter, bebas_neue } from '@/app/fonts';
+import Link from 'next/link';
+
+//Estou considerando que esta page.tsx é a do administrador.
+export default function Login() {
+  return (
+    <Container>
+      <AuthHeader />
+      <Main>
+        <AuthForm />
+      </Main>
+    </Container>
+  );
+}
