580c1fb463351829056f151c2a17f76c36b84da4
Pavanzinho <dms.cerberus@hotmail.com>
2023-07-25 10:17:26 -0300

    feat:create next aplication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
 'use client';
 
-import Link from 'next/link';
-
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
-import { AppRoutes } from './routes/appRoutes';
-
 //Estou considerando que esta page.tsx é a do administrador.
 export default function Home() {
   return <></>;
