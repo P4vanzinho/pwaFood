1934a2f5c6efc12111a1741ec4f1dfb2274d8693
Pavanzinho <dms.cerberus@hotmail.com>
2023-07-25 10:35:14 -0300

    feat: adding font file

---
+++ src/fonts.ts
@@ -0,0 +1,13 @@
+import { Bebas_Neue, Poppins } from 'next/font/google';
+
+export const bebas_neue = Bebas_Neue({
+  display: 'swap',
+  weight: '400',
+  subsets: ['latin'],
+});
+
+export const poppins = Poppins({
+  display: 'swap',
+  weight: '500',
+  subsets: ['latin'],
+});
