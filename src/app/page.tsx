0e97c4645a66ac21b6c0d44a34af0ad4226eb791
Pavanzinho <dms.cerberus@hotmail.com>
2023-07-25 10:05:11 -0300

    feat: create next aplication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,12 @@
-'use client'
+'use client';
 
-import Link from "next/link"
+import Link from 'next/link';
 
 import { useRouter } from 'next/navigation';
-import { useEffect } from "react";
-import { AppRoutes } from "./routes/appRoutes";
-
-
+import { useEffect } from 'react';
+import { AppRoutes } from './routes/appRoutes';
 
 //Estou considerando que esta page.tsx é a do administrador.
 export default function Home() {
-  const { push } = useRouter()
-
-
-
-  useEffect(() => {
-    push(AppRoutes.public.login)
-  }, [])
-
-  return (
-    <></>
-  )
+  return <></>;
 }
