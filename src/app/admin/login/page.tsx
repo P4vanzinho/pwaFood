3b38b3a4483327c238045f061063900732f9b691
Pavanzinho <dms.cerberus@hotmail.com>
2023-07-25 21:39:58 -0300

    chore: config husky

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -2,7 +2,7 @@
 
 import { SignInForm, Container, Main } from './styles';
 
-import AuthForm from '../../components/AuthForm';
+import AuthForm from '../../components/authForm';
 import AuthHeader from '../../components/AuthHeader';
 
 import Image from 'next/image';
