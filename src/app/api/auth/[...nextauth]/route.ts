cfb2f8baaa1cb1d6f6b81bcbef8697d04478121b
Pavanzinho <dms.cerberus@hotmail.com>
2023-08-02 16:32:12 -0300

    feat: create authentication routes

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,64 @@
+import NextAuth, { NextAuthOptions } from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import GoogleProvider from 'next-auth/providers/google';
+
+const nextAuthOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: 'credentials',
+      credentials: {
+        email: { label: 'email', type: 'email' },
+        password: { label: 'password', type: 'password' },
+      },
+
+      async authorize(credentials, req) {
+        const response = await fetch(
+          'https://goldfish-app-vg4r3.ondigitalocean.app/tenant/login',
+          {
+            method: 'POST',
+            headers: {
+              'Content-type': 'application/json',
+            },
+            body: JSON.stringify({
+              email: credentials?.email,
+              password: credentials?.password,
+            }),
+          },
+        );
+
+        const body = await response.json();
+
+        if (body.data) {
+          return body;
+        } else {
+          throw new Error(body.error);
+          //esse erro vai cair la no result, quando eu for usar useS
+        }
+      },
+    }),
+
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+    }),
+  ],
+
+  callbacks: {
+    async jwt({ token, user }) {
+      user && (token.user = user);
+      return token;
+    },
+    async session({ session, token }) {
+      session = token.user as any;
+      return session;
+    },
+  },
+
+  pages: {
+    signIn: '/auth/login',
+  },
+};
+
+const handler = NextAuth(nextAuthOptions);
+
+export { handler as GET, handler as POST, nextAuthOptions };
