07d227e6a7f548c1478d435a10b2a938b9b12da1
Pavanzinho <dms.cerberus@hotmail.com>
2023-08-04 15:14:10 -0300

    feat: create foodFetch function

---
+++ src/app/services/foodFetch/foodFetch.ts
@@ -0,0 +1,38 @@
+import { toast } from 'react-toastify';
+
+type FoodFetchProps<T> = {
+  body: unknown;
+  headers?: Record<string, string>;
+  method?: string;
+  endPoint: string;
+};
+
+export async function foodFetch<T>({
+  body,
+  headers,
+  method,
+  endPoint,
+}: FoodFetchProps<T>): Promise<null | T> {
+  console.log(process.env.NEXT_PUBLIC_BASE_URL_API);
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL_API}/${endPoint}`,
+    {
+      method: method ?? 'GET',
+      headers: headers ?? {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    },
+  );
+
+  const responseBody = await response.json();
+  console.log(responseBody);
+
+  if (responseBody.error) {
+    toast.error(responseBody.error, { draggable: false });
+  } else {
+    toast.success(responseBody.message);
+  }
+
+  return responseBody as T;
+}
