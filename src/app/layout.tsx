049d69dbca8f0194515ba6464093addbabccb233
Pavanzinho <dms.cerberus@hotmail.com>
2023-08-04 15:06:56 -0300

    refactor: delete descessary modules

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { ReactNode } from 'react';
-import { useRouter } from 'next/navigation';
 
 import StyledComponentsRegistry from '../lib/registry';
 
@@ -9,14 +8,14 @@ import { bebas_neue } from './fonts';
 
 import { createGlobalStyle } from 'styled-components';
 
-import { Metadata } from 'next';
-
 import { ThemeProvider } from 'styled-components';
 
 import { theme } from './styles/theme';
 
-import NextAuthSessionProvider from './Providers/SessionProvider';
+import NextAuthSessionProvider from './providers/SessionProvider';
+
 import { ToastContainer } from 'react-toastify';
+
 import '../assets/ReactToastify.css';
 
 const GlobalStyle = createGlobalStyle`
