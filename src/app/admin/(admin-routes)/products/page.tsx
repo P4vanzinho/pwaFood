7f53e6c41b2488bf89d210b12ee2bd3be156aed7
Pavanzinho <dms.cerberus@hotmail.com>
2023-08-05 14:17:05 -0300

    refactor: return page for stage

--- src/app/admin/(admin-routes)/products/page.tsx
+++ src/app/admin/(admin-routes)/products/page.tsx
@@ -1,49 +1,13 @@
 'use client';
+import { Container } from './styles';
 
 import { EndpointFoodApiEnum } from '@/app/enums';
 import useFoodFetch from '@/app/hooks/useFoodFetch';
 
 export default function Products() {
-  const {
-    data: categories,
-    loading,
-    setQueryBuilder,
-  } = useFoodFetch(EndpointFoodApiEnum.PRODUCT_CATEGORY, {
-    injectProducts: false,
-  });
-
-  const { request: requestRegisterCategory, error } = useFoodFetch();
-
-  const onClick = () => {
-    setQueryBuilder({
-      injectProducts: true,
-    });
-
-    /*  requestRegisterCategory({
-      method: 'POST',
-      body: {
-        businessId: 21,
-        name: 'Nova Categoria do ro',
-      },
-      endPoint: EndpointFoodApiEnum.PRODUCT_CATEGORY,
-    }); */
-  };
-
   return (
-    <>
-      {loading && <div>loading</div>}
-
-      {error && <div>error</div>}
-
-      {categories?.length &&
-        categories.map(category => (
-          <div>
-            <h1>{category.name}</h1>
-            {category.product?.map(product => product.name)}
-          </div>
-        ))}
-
-      <button onClick={onClick}>MEU BOTAOZAO</button>
-    </>
+    <Container>
+      <h1>products page</h1>
+    </Container>
   );
 }
