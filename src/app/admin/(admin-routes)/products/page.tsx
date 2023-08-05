c3227a4776e7fe5ba5dc6c2f4686668df419e8a1
Pavanzinho <dms.cerberus@hotmail.com>
2023-08-05 20:43:01 -0300

    feat: create condition of render in page

--- src/app/admin/(admin-routes)/products/page.tsx
+++ src/app/admin/(admin-routes)/products/page.tsx
@@ -1,13 +1,29 @@
 'use client';
-import { Container } from './styles';
-
-import { EndpointFoodApiEnum } from '@/app/enums';
-import useFoodFetch from '@/app/hooks/useFoodFetch';
+import {
+  Container,
+  ContentContainer,
+  NotListProductContainer,
+  Main,
+} from './styles';
+import AdminAppHeader from '@/app/components/AdminAppHeader';
+import { poppins, inter, bebas_neue } from '@/app/fonts';
 
 export default function Products() {
+  let categoryOfProducts = true;
+
   return (
     <Container>
-      <h1>products page</h1>
+      <AdminAppHeader />
+      {categoryOfProducts ? (
+        <Main>oi</Main>
+      ) : (
+        <NotListProductContainer>
+          <p className={poppins.className}>
+            Voce ainda nao tem nenhuma categoria cadastrada. Comece criando uma
+            categoria <span>clicando aqui!</span>
+          </p>
+        </NotListProductContainer>
+      )}
     </Container>
   );
 }
