530848658189bbeaa139dbc78318fa1e8bf4fe1f
Pavanzinho <dms.cerberus@hotmail.com>
2023-09-04 16:37:56 -0300

    feat: add redirect of login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
+import { useSession } from 'next-auth/react';
+
 export default function Home() {
+  const router = useRouter();
+
+  router.push('/admin/login');
+
   return <></>;
 }
