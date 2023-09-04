31ed11e9aef861f256d70b23be246ac2a0a2afba
Pavanzinho <dms.cerberus@hotmail.com>
2023-09-04 16:04:30 -0300

    fix: delete desnecessary lines in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,5 @@
 'use client';
-import { useRouter } from 'next/navigation';
-import { useSession } from 'next-auth/react';
-export default function Home() {
-  const { data: session } = useSession();
-  const router = useRouter();
 
-  if (session) {
-    router.replace('/admin/login');
-  }
+export default function Home() {
   return <></>;
 }
