375fa3278ffdf35e3e4a510a5dd04e6c6247c8bc
Pavanzinho <dms.cerberus@hotmail.com>
2023-09-04 16:08:09 -0300

    feat: add configs in nextConfig , for images

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'fooda.nyc3.digitaloceanspaces.com',
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig;
