b6de147e91cea549596abf180bb9f9fa42db4637
Pavanzinho <dms.cerberus@hotmail.com>
2023-09-10 06:42:11 -0300

    refactor: delete desnecessary modules

--- src/app/admin/(admin-routes)/layout.tsx
+++ src/app/admin/(admin-routes)/layout.tsx
@@ -1,4 +1,8 @@
+import { getServerSession } from 'next-auth';
 import { ReactNode } from 'react';
+import { authOptions } from '@/app/api/auth/authOptions';
+import { redirect } from 'next/navigation';
+
 interface PrivateLayoutProps {
   children: ReactNode;
 }
