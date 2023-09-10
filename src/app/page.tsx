c15c93361b9bc478ba4038cce6e704e125aac807
Pavanzinho <dms.cerberus@hotmail.com>
2023-09-10 06:42:27 -0300

    refactor: delete desnecessary modules and lines

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,5 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
-import { useSession } from 'next-auth/react';
-
 export default function Home() {
-  const router = useRouter();
-
-  router.push('/admin/login');
-
   return <></>;
 }
