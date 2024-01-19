0b027ef4cbb2521c1ef798a7d1cfbc9f3cbd5f05
Pavanzinho <thiago772000@gmail.com>
2024-01-19 09:29:06 -0300

    feat : create  handleNewProduct and emptyingValues function

--- src/app/components/ProductPage/index.tsx
+++ src/app/components/ProductPage/index.tsx
@@ -14,13 +14,18 @@ import {
   InputCheckBoxInToggle,
   Switch,
   ButtonsContainer,
-  PriceLabel,
   ImageWithoutUpload,
   ImageWithUpload,
 } from './styles';
 
 import Title from '@/app/components/Title';
-import { ChangeEvent, useEffect, useRef, useState } from 'react';
+import {
+  ChangeEvent,
+  SyntheticEvent,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 import Image from 'next/image';
 
 import { MdOutlineKeyboardArrowDown } from 'react-icons/md';
@@ -43,23 +48,6 @@ type ProductPageProps = {
   mode?: 'private' | 'public';
 };
 
-interface Category {
-  id: number;
-  name: string;
-  businessId: number;
-  uploadId: number;
-}
-
-type CategoryListProps = {
-  businessId: number | string;
-  mode?: 'private' | 'public';
-};
-
-interface Upload {
-  id: number;
-  name: string;
-}
-
 export default function ProductPage({
   props,
   businessId,
@@ -71,15 +59,13 @@ export default function ProductPage({
   const title =
     modePage === `register` ? `CADASTRO DE PRODUTO` : `EDIÇÃO DE PRODUTO`;
 
-  const fileTypes = ['JPG', 'PNG', 'GIF'];
-
   const [foodTitle, setFoodTitle] = useState<string>(``);
-
   const textAreaMaxLength = 300;
   const [remaining, setRemaining] = useState<number>(textAreaMaxLength);
   const [checked, setChecked] = useState<boolean>(false);
   const [price, setPrice] = useState<string>('');
   const [priceIsValid, setPriceIsValid] = useState<boolean>(false);
+  const [description, setDescription] = useState<string>('');
   const [category, setCategory] = useState<string>('');
   const [categorySelected, setCategorySelected] = useState<
     FoodApiCategory | undefined
@@ -88,6 +74,36 @@ export default function ProductPage({
   const [uploadName, setUploadName] = useState<string | undefined>('');
   const [isUploadedImage, setIsUploadedImage] = useState<boolean>(false);
   const [file, setFile] = useState<File | null>(null);
+
+  const emptyingValues = () => {
+    setFoodTitle(``);
+    setChecked(false);
+    setPrice(``);
+    setDescription(``);
+    setCategory(``);
+    setCategorySelected(undefined);
+    setUploadName(``);
+    setIsUploadedImage(false);
+    setFile(null);
+  };
+
+  useEffect(() => {
+    if (!file) return;
+
+    const formData = new FormData();
+    formData.append('upload', file, file.name);
+
+    requestNewProductUpload({
+      method: 'POST',
+      body: formData,
+      headers: {
+        'Content-type': 'multipart/form-data',
+      },
+      endPoint: EndpointFoodApiEnum.UPLOAD,
+    });
+
+    console.log(`upload`, upload);
+  }, [file]);
 
   //Categories request
   const { data: categories } = useFoodFetch<FoodApiCategory[]>(
@@ -106,6 +122,10 @@ export default function ProductPage({
     loading: loadingNewProductUpload,
     data: upload,
   } = useFoodFetch<FoodApiUpload>();
+
+  //Register product request
+  const { request: requestRegisterNewProduct, loading: newProductLoading } =
+    useFoodFetch();
 
   function handleTextAreaLength(event: ChangeEvent<HTMLTextAreaElement>) {
     setRemaining(textAreaMaxLength - event.target.value.length);
@@ -131,6 +151,28 @@ export default function ProductPage({
   function handleRemoveUpload() {
     setUploadName('');
     setIsUploadedImage(() => false);
+  }
+
+  function handleNewProduct() {
+    requestRegisterNewProduct({
+      method: 'POST',
+      body: {
+        categoryId: categorySelected?.id,
+        name: foodTitle,
+        price,
+        description,
+        uploadId: upload?.id,
+        enabled: checked,
+      },
+      endPoint: EndpointFoodApiEnum.PRODUCT,
+    });
+
+    emptyingValues();
+  }
+
+  function handleSubmit(event: SyntheticEvent) {
+    event.preventDefault();
+    handleNewProduct();
   }
 
   //Selecionando categoria
@@ -170,162 +212,166 @@ export default function ProductPage({
   }, [file]);
 
   return (
-    <FormContainer>
-      <div className="takeColor">
-        <h1>titulo</h1>
-      </div>
+    <Container>
+      <FormContainer>
+        <Title>{title}</Title>
+        <form onSubmit={handleSubmit}>
+          <fieldset>
+            <Input
+              label="TITULO"
+              className={poppins.className}
+              id="foodTitle"
+              type="text"
+              placeholder="Dê um título ao seu produto"
+              onChange={e => setFoodTitle(e.target.value)}
+              value={foodTitle}
+            />
+            {/* </FoodTitleLabel> */}
 
-      <Title>{title}</Title>
-      <fieldset>
-        <Input
-          label="TITULO"
-          className={poppins.className}
-          id="foodTitle"
-          type="text"
-          placeholder="Dê um título ao seu produto"
-          onChange={e => setFoodTitle(e.target.value)}
-          value={foodTitle}
-        />
-        {/* </FoodTitleLabel> */}
+            {isUploadedImage ? (
+              <ImageProductContainer>
+                <ImageWithUpload>
+                  <p className={poppins.className}>
+                    Como será visualizado pelo cliente
+                  </p>
 
-        {isUploadedImage ? (
-          <ImageProductContainer>
-            <ImageWithUpload>
-              <p className={poppins.className}>
-                Como será visualizado pelo cliente
-              </p>
-
-              <AvatarEditor
-                image={`https://fooda.nyc3.digitaloceanspaces.com/develop/${uploadName}`}
-                height={127}
-                width={127}
-                scale={1}
-                className="canvasDimensions"
-              />
-
-              <div>
-                <button className={poppins.className}> EDITAR</button>
-                <button
-                  className={poppins.className}
-                  onClick={handleRemoveUpload}
-                >
-                  REMOVER
-                </button>
-              </div>
-            </ImageWithUpload>
-          </ImageProductContainer>
-        ) : (
-          <Dropzone
-            noKeyboard
-            onDrop={acceptedFiles => onChangeUploadFile(acceptedFiles)}
-          >
-            {({ getRootProps, getInputProps }) => (
-              <ImageProductContainer {...getRootProps()}>
-                <ImageWithoutUpload>
-                  <Image
-                    src={'/productImage.png'}
-                    alt="Imagem exemplo"
-                    height={51}
-                    width={51}
+                  <AvatarEditor
+                    image={`https://fooda.nyc3.digitaloceanspaces.com/develop/${uploadName}`}
+                    height={127}
+                    width={127}
+                    scale={1}
+                    className="canvasDimensions"
                   />
-                  <p className={poppins.className}>Arraste uma imagem</p>
-                  <div>
-                    <div></div>
-                    <span className={poppins.className}>ou</span>
-                    <div></div>
-                  </div>
-                  <button className={poppins.className}>
-                    <input
-                      onChange={onChangeUploadFile}
-                      type="file"
-                      {...getInputProps()}
-                    />
-                    Selecione do computador
-                  </button>
-                </ImageWithoutUpload>
-              </ImageProductContainer>
-            )}
-          </Dropzone>
-        )}
 
-        <SelectCategoryLabel
-          htmlFor="foodCategory"
-          className={bebas_neue.className}
-        >
-          SELECIONE EM QUAL CATEGORIA ESTE PRODUTO SERA EXIBIDO
-          <SelectContainer>
-            <select
-              className={poppins.className}
-              value={category}
-              id="foodCategory"
-              onChange={e => setCategory(e.target.value)}
+                  <div>
+                    <button className={poppins.className}> EDITAR</button>
+                    <button
+                      className={poppins.className}
+                      onClick={handleRemoveUpload}
+                    >
+                      REMOVER
+                    </button>
+                  </div>
+                </ImageWithUpload>
+              </ImageProductContainer>
+            ) : (
+              <Dropzone
+                noKeyboard
+                onDrop={acceptedFiles => onChangeUploadFile(acceptedFiles)}
+              >
+                {({ getRootProps, getInputProps }) => (
+                  <ImageProductContainer {...getRootProps()}>
+                    <ImageWithoutUpload>
+                      <Image
+                        src={'/productImage.png'}
+                        alt="Imagem exemplo"
+                        height={51}
+                        width={51}
+                      />
+                      <p className={poppins.className}>Arraste uma imagem</p>
+                      <div>
+                        <div></div>
+                        <span className={poppins.className}>ou</span>
+                        <div></div>
+                      </div>
+                      <button className={poppins.className}>
+                        <input
+                          onChange={onChangeUploadFile}
+                          type="file"
+                          {...getInputProps()}
+                        />
+                        Selecione do computador
+                      </button>
+                    </ImageWithoutUpload>
+                  </ImageProductContainer>
+                )}
+              </Dropzone>
+            )}
+
+            <SelectCategoryLabel
+              htmlFor="foodCategory"
+              className={bebas_neue.className}
             >
-              {/*obs: testar o que realmente e necessario nesses ifs*/}
-              {businessId &&
-                categories &&
-                categories?.map(category => (
-                  <option key={category.id} value={category.name}>
-                    {category.name}
-                  </option>
-                ))}
-            </select>
-            <MdOutlineKeyboardArrowDown size={24} />
-          </SelectContainer>
-        </SelectCategoryLabel>
-        <DescriptionLabel
-          htmlFor="description"
-          className={bebas_neue.className}
-        >
-          DESCRICAO
-          <div>
-            <textarea
-              id="description"
-              maxLength={textAreaMaxLength}
-              className={poppins.className}
-              onInput={handleTextAreaLength}
-              placeholder="Exemplo: 2 carnes de hamburger de angus, alface americana,
+              SELECIONE EM QUAL CATEGORIA ESTE PRODUTO SERA EXIBIDO
+              <SelectContainer>
+                <select
+                  className={poppins.className}
+                  value={category}
+                  id="foodCategory"
+                  onChange={e => setCategory(e.target.value)}
+                >
+                  {/*obs: testar o que realmente e necessario nesses ifs*/}
+                  {businessId &&
+                    categories &&
+                    categories?.map(category => (
+                      <option key={category.id} value={category.name}>
+                        {category.name}
+                      </option>
+                    ))}
+                </select>
+                <MdOutlineKeyboardArrowDown size={24} />
+              </SelectContainer>
+            </SelectCategoryLabel>
+            <DescriptionLabel
+              htmlFor="description"
+              className={bebas_neue.className}
+            >
+              DESCRICAO
+              <div>
+                <textarea
+                  value={description}
+                  id="description"
+                  maxLength={textAreaMaxLength}
+                  className={poppins.className}
+                  onInput={handleTextAreaLength}
+                  onChange={event => setDescription(event.target.value)}
+                  placeholder="Exemplo: 2 carnes de hamburger de angus, alface americana,
                   catupiry original, molho especial, pao da hora e queijo de
                   avestruz"
-            ></textarea>
+                ></textarea>
 
-            <span className={poppins.className}>
-              Restam {remaining} caracteres
-            </span>
-          </div>
-        </DescriptionLabel>
+                <span className={poppins.className}>
+                  Restam {remaining} caracteres
+                </span>
+              </div>
+            </DescriptionLabel>
 
-        <ToggleSwitchContainer>
-          <ToggleSwitch>
-            <p className={poppins.className}>Exibir o produto no cardapio</p>
-            <LabelToglleSwitch>
-              <InputCheckBoxInToggle
-                type="checkbox"
-                onChange={() => setChecked(!checked)}
-              />
-              <Switch />
-            </LabelToglleSwitch>
-          </ToggleSwitch>
-        </ToggleSwitchContainer>
+            <ToggleSwitchContainer>
+              <ToggleSwitch>
+                <p className={poppins.className}>
+                  Exibir o produto no cardapio
+                </p>
+                <LabelToglleSwitch>
+                  <InputCheckBoxInToggle
+                    type="checkbox"
+                    onChange={() => setChecked(!checked)}
+                  />
+                  <Switch />
+                </LabelToglleSwitch>
+              </ToggleSwitch>
+            </ToggleSwitchContainer>
 
-        <Input
-          className={poppins.className}
-          id="price"
-          type="text"
-          placeholder="Exemplo: 15,25"
-          onChange={e => setPrice(e.target.value)}
-          value={price}
-          onInput={handleInputPrice}
-        />
+            <Input
+              className={poppins.className}
+              id="price"
+              type="text"
+              placeholder="Exemplo: 15,25"
+              onChange={e => setPrice(e.target.value)}
+              value={price}
+              onInput={handleInputPrice}
+            />
 
-        {priceIsValid && (
-          <span> O preco de seu prato precisa ser um número</span>
-        )}
-      </fieldset>
+            {priceIsValid && (
+              <span> O preco de seu prato precisa ser um número</span>
+            )}
+          </fieldset>
 
-      <ButtonsContainer>
-        <Button text={`Cancelar`} />
-        <Button text={`Salvar`} />
-      </ButtonsContainer>
-    </FormContainer>
+          <ButtonsContainer>
+            <Button text={`Cancelar`} />
+            <Button text={`Salvar`} type="submit" />
+          </ButtonsContainer>
+        </form>
+      </FormContainer>
+    </Container>
   );
 }
